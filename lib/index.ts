5efccb41e4e31985af18ecc91696b5599af740af
githop <tom@githop.com>
2016-07-23 22:50:00 -0600

    updates(index.ts): add http request

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,32 @@
 const fs = require('fs');
 const {join: pjoin} = require('path');
+const request = require('request');
 const Task = require('data.task');
-const {sequence, map, toUpper, compose} = require('ramda');
+const {sequence, map, toUpper, compose, prop, append} = require('ramda');
 
 const filePath = (file) => (pjoin(__dirname, '../', file));
 const logErr = err => console.log(`Error: ${err}`);
 const logData = data => console.log('Data', data);
-const existy = x => x != null;
+const exists = x => x != null;
 
 const taskifyReadFile = (path) => {
   return new Task((reject, resolve) => {
-    fs.readFile(path, 'utf8', (err, data) => existy(err) ? reject(err) : resolve(data));
+    fs.readFile(path, 'utf8', (err, data) => exists(err) ? reject(err) : resolve(data));
   });
 };
 
-const fNames = ['a.txt', 'b.txt', 'c.txt'];
+const taskifyReq = (url) => {
+  return new Task((reject, resolve) => {
+    request(url, (err, resp, body) => {
+      exists(err) ? reject(err) : resolve(JSON.parse(body));
+    });
+  });
+};
 
-const composeIt = compose(map(taskifyReadFile), map(filePath));
-const filesRead = composeIt(fNames);
-const seqd = sequence(Task.of, filesRead);
-const done = map(map(toUpper), seqd);
+const getFilesAsTasks = compose(map(taskifyReadFile), map(filePath));
+const getSkywalker = taskifyReq('http://swapi.co/api/people/1').map(prop('name'));
+const files = getFilesAsTasks(['a.txt', 'b.txt', 'c.txt']);
+const seq = sequence(Task.of, append(getSkywalker, files));
+const done = map(map(toUpper), seq);
 
 done.fork(logErr, logData);
