f612846962ae1402533d184eebaa8c830f8c6f85
githop <tom@githop.com>
2016-07-24 01:23:54 -0600

    refactor(index.ts): traverse over sequence for easier transformation chore(types): begin adding types

--- lib/index.ts
+++ lib/index.ts
@@ -2,20 +2,20 @@ const fs = require('fs');
 const {join: pjoin} = require('path');
 const request = require('request');
 const Task = require('data.task');
-const {sequence, map, toUpper, compose, prop, append} = require('ramda');
+const {map, toUpper, compose, prop, append, traverse} = require('ramda');
 
-const filePath = (file) => (pjoin(__dirname, '../', file));
-const logErr = err => console.log(`Error: ${err}`);
-const logData = data => console.log('Data', data);
-const exists = x => x != null;
+const filePath = (file:string):string => (pjoin(__dirname, '../', file));
+const logErr = (err):void => console.log(`Error: ${err}`);
+const logData = (data):void => console.log('Data', data);
+const exists = (x):boolean => x != null;
 
-const taskifyReadFile = (path) => {
+const taskifyReadFile = (path:string) => {
   return new Task((reject, resolve) => {
     fs.readFile(path, 'utf8', (err, data) => exists(err) ? reject(err) : resolve(data));
   });
 };
 
-const taskifyReq = (url) => {
+const taskifyReq = (url:string) => {
   return new Task((reject, resolve) => {
     request(url, (err, resp, body) => {
       exists(err) ? reject(err) : resolve(JSON.parse(body));
@@ -24,9 +24,8 @@ const taskifyReq = (url) => {
 };
 
 const getFilesAsTasks = compose(map(taskifyReadFile), map(filePath));
-const getSkywalker = map(prop('name'), taskifyReq('http://swapi.co/api/people/1'));
 const files = getFilesAsTasks(['a.txt', 'b.txt', 'c.txt']);
-const seq = sequence(Task.of, append(getSkywalker, files));
-const done = map(map(toUpper), seq);
+const getSkywalker = map(prop('name'), taskifyReq('http://swapi.co/api/people/1'));
+const done = traverse(Task.of, map(toUpper), append(getSkywalker, files));
 
 done.fork(logErr, logData);
